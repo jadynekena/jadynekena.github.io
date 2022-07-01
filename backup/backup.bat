81540c7bee8a84f4ad252372d3980690724576dc
Jady Nekena <contact@data-addict.com>
2022-07-01 03:37:00 +0200

    back up through site

--- backup/backup.bat
+++ backup/backup.bat
@@ -1,6 +1,5 @@
 cd "D:\Jady\Auto Entreprise Jady\jadynekena.com"
-start "my_jekyll_server" cmd /k "jekyll serve & exit"
-start msedge "http://localhost:4000/backup/"
+start msedge "http://jadynekena.com/backup/"
 timeout 2
 taskkill /IM "msedge.exe" /F
 timeout 2
