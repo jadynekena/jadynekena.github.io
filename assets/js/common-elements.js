056f417442e2f6017d5c134b1aec82fcf8c22c11
Jady Nekena <contact@data-addict.com>
2022-07-04 04:48:45 +0200

    no blocking stuff

--- assets/js/common-elements.js
+++ assets/js/common-elements.js
@@ -1691,11 +1691,13 @@ function switch_light(){
 	let one_more_class = loc().includes("/DATAVIZ/") ? ' dataviz' : ''
 	document.querySelector('body').className = next === "🌙" ? "night" + one_more_class : ""
 
-	if(next === '🌙'){
-		if(local_if_dataviz_not_iframe()){
-			$('body').addClass('local')
-		}else{
-			$('body').remove('local')
+	if(loc().includes("/DATAVIZ/")){
+		if(next === '🌙'){
+			if(local_if_dataviz_not_iframe()){
+				$('body').addClass('local')
+			}else{
+				$('body').remove('local')
+			}
 		}
 	}
 
